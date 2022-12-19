f393d95b9a7c5323cd47dd447da074dfa220ae5b
AmrAmin761 <85031098+AmrAmin761@users.noreply.github.com>
2022-12-19 23:46:16 +0200

    Added the fucking methods implementations (statement,repeat_stm,assign_stmt,exp)

--- Parser.cpp
+++ Parser.cpp
@@ -1,3 +1,4 @@
+
 #include "Parser.hpp"
 
 unsigned int index = 0;
@@ -11,5 +12,61 @@ void match(string input, vector<Token> Tokens){//needs to be refactored
     else
         error = true;
 
+}
+
+SyntaxTree* Parser::program (vector<Token> Tokens){
+    SyntaxTree* node = stmt_sequence(Tokens);
+    return node;
+}
+
+SyntaxTree* statement (vector<Token> Tokens){
+    SyntaxTree* node = NULL;
+    Token current_token;
+    if(index < Tokens.size())
+        current_token = Tokens[index];
+    if( current_token.Type == "IF" ){
+        node = if_stmt(Tokens);
+    } else if(current_token.Type == "IDENTIFIER"){
+        node = assign_stmt(Tokens);
+    } else if (current_token.Type == "READ")
+        node = read_stmt(Tokens);
+    else if (current_token.Type == "WRITE")
+        node = write_stmt(Tokens);
+
+    return node;
+}
+
+SyntaxTree* repeat_stmt (vector<Token> Tokens){
+    SyntaxTree* node = new SyntaxTree();
+    match("REPEAT", Tokens);
+    node->setType(REPEAT_STATEMENT);
+    node->addChild(stmt_sequence(Tokens));
+    match("UNTIL", Tokens);
+    node->addChild(exp(Tokens));
+    return node;
+}
+
+SyntaxTree* assign_stmt (vector<Token> Tokens){
+    SyntaxTree* node = new SyntaxTree();
+    node->setType(ASSIGN_STATEMENT);
+    match("IDENTIFIER",Tokens);
+    match("ASSIGN", Tokens);
+    node->addChild(exp(Tokens);
+    return node;
+}
+
+SyntaxTree* exp (vector<Token> Tokens){
+    SyntaxTree* cur, *c1 = simple_exp(Tokens), *c2;
+    Token x;
+    if (index < Tokens.size())
+        x = Tokens[index];
+    if (x.Type == "LESSTHAN" || x.Type == "EQUAL") {
+        cur = comparison_op(Tokens);
+        cur->addChild(c1);
+        c2 = simple_exp(Tokens);
+        cur->addChild(c2);
+        c1 = cur;
+    }
+    return c1;
 }
 
